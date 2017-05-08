7d1026811f7ea4a25c77cbd0828530b4b4821bd0
Soh Yu Ming <sohymg@gmail.com>
2017-05-08 22:03:25 +0800

    Add offline mode

---
+++ service_worker.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var CACHE_NAME = 'cache-v1';
+var urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', function(event) {
+	// Open device cache and store our list of items
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+	// Intercept fetch request
+  event.respondWith(
+  	// match and serve cached asset if it exists
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+  	// Open our apps cache and delete any old cache items
+  	caches.open(CACHE_NAME).then(function(cacheNames){
+  		cacheNames.keys().then(function(cache){
+  			cache.forEach(function(element, index, array) {
+  				if (urlsToCache.indexOf(element) === -1){
+  					caches.delete(element);
+  				}
+  	    });
+  		});
+  	})
+  );
+});
