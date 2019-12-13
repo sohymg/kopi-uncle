2ada756a5125bfcf67f03af6b75b98f3d8450b55
Soh Yu Ming <sohymg@estl.sg>
2019-12-13 15:34:29 +0800

    Fix service worker to show page even if offline

--- service_worker.js
+++ service_worker.js
@@ -16,13 +16,26 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-	// Intercept fetch request
-  event.respondWith(
-  	// match and serve cached asset if it exists
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+  // https://deanhume.com/create-a-really-really-simple-offline-page-using-service-workers/
+  
+  // request.mode = navigate isn't supported in all browsers
+  // so include a check for Accept: text/html header.
+  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+    event.respondWith(
+      fetch(event.request.url).catch(error => {
+          // Return the offline page
+          return caches.match('index.html');
+      })
+    );
+  }
+  else{
+      // Respond with everything else if we can
+      event.respondWith(caches.match(event.request)
+        .then(function (response) {
+            return response || fetch(event.request);
+        })
+      );
+    }
 });
 
 self.addEventListener('activate', function(event) {
